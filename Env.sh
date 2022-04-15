7ed729739c117ef4e1a75d84a701dd22a89361fc
Mochen <mochen20070702@gmail.com>
2022-04-15 21:26:38 +0800

    Env

---
+++ Env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+workdir="$GITHUB_WORKSPACE"
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y gcc g++ python make texinfo texlive bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev unzip language-pack-zh-hans
+sudo apt install -y --no-install-recommends git make bc bison openssl \
+    curl zip kmod cpio flex libelf-dev libssl-dev libtfm-dev wget \
+    device-tree-compiler ca-certificates python3 python2
+sudo apt install bc \
+            binutils-dev \
+            bison \
+            build-essential \
+            ca-certificates \
+            ccache \
+            clang \
+            cmake \
+            curl \
+            file \
+            flex \
+            git \
+            libelf-dev \
+            libssl-dev \
+            lld \
+            make \
+            ninja-build \
+            python3-dev \
+            texinfo \
+            u-boot-tools \
+            xz-utils \
+            zlib1g-dev
